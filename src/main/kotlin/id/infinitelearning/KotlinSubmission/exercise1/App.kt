92f7c596093e03414ddf24415bac7e3b949df5e8
jantul <115470217+jantul@users.noreply.github.com>
2022-10-23 23:07:45 +0700

    KotlinArif1

---
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise1/App.kt
@@ -0,0 +1,88 @@
+package id.infinitelearning.KotlinSubmission.exercise1
+
+/**
+Latihan 1
+Lengkapi fungsi myProfile di bawah ini dengan membuat variabel dengan ketentuan dibawah ini:
+- Variable bertipe data string yang menyimpan nilai nama depan Anda.
+- Variable bertipe data string yang menyimpan nilai nama belakang Anda.
+- Variable bertipe data number yang menyimpan nilai umur Anda.
+- Variable bertipe data boolean yang menyimpan nilai status Anda (single atau tidak)
+Dan cetak setiap variabel ke layar saat variable myProfile di panggil
+ */
+fun myProfile() {
+    val fristName : String = "Muhammad Arif"
+    println("My fist Name is : $fristName")
+
+    val lastName : String ="Sirait"
+    println("My Last Name is : $lastName")
+
+    val age : Int = 22
+    println("My Last Age is : $age")
+
+    val sex : String = "Pria"
+    println("My Gender is : $sex")
+
+    val x: Boolean =false
+    val y: Boolean =true
+
+    println()
+    println("status kamu adalah = " + (x && y))
+    println("status kamu adalah = " + (x || y))
+    println("status kamu adalah = " + (!y))
+
+}
+/**
+ *  Latihan 2
+ *  Lengkapi fungsi di bawah ini agar dapat mencetak nilai dari parameter-parameter yang ada dengan fungsi println
+ */
+fun groupDetail(groupId: String, groupMember: List<Any>, session: String): Any {
+    println(groupId)
+    println(groupMember)
+    println(session)
+    return arrayOf(groupId, groupMember, session)
+}
+/**
+ * Latihan 3
+ * Buat variable yang berisi daftar anggota group kamu,
+ * Kemudian akses item yang berisi nama Anda dari variable tersebut, lalu jadikan nilai kembalian untuk fungsi myName ini
+ *
+ */
+fun myName(): String {
+    return "Muhammad Arif Sirait"
+
+
+}
+
+/**
+ * Latihan 4
+ * Ubah nilai kembalian untuk fungsi totalMember ini dengan rumus:
+ *
+ *      total mentor + jumlah anggota group
+ *
+ */
+fun totalMember(): Int {
+    val mentor = arrayOf("Dian", "Sani", "Ardy", "Reza")
+    val countOfGroup = 6
+
+    return mentor.count() + countOfGroup;
+
+}
+
+fun main() {
+
+    myProfile()
+
+    val myName = myName()
+    println("My Name is: $myName")
+
+    val totalMember = totalMember()
+    println("Total Member group + Mentor: $totalMember")
+
+    /**
+     *  Latihan 5
+     *  Ubah nilai argumen-argumen dari fungsi groupDetail di bawah ini sesuai dengan data group kamu
+     *
+     */
+    groupDetail("Group 4",listOf("6 member", "Infinite Learning", "Productivity"), "Afternoon")
+
+}
